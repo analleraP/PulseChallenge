59519eb1bc5477922030d282a141bcaf3055e3d8
Ana Elisa Llera <ana.elisa.llera@gmail.com>
2018-05-01 11:40:02 +0200

    add data entities

---
+++ app/src/main/java/com/allerap/android/pulsechallenge/data/entities/Error.kt
@@ -0,0 +1,3 @@
+package com.allerap.android.pulsechallenge.data.entities
+
+data class Error(val message: String)
---
+++ app/src/main/java/com/allerap/android/pulsechallenge/data/entities/Item.kt
@@ -0,0 +1,9 @@
+package com.allerap.android.pulsechallenge.data.entities
+
+data class Item(
+        val id: Int,
+        val title: String,
+        val subtitle: String,
+        val body: String,
+        val date: String
+)
